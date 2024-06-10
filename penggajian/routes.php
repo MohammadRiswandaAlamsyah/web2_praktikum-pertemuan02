1011ddc08c0fbe77a7444e01c6495241f470f227
Mohammad Riswanda Alamsyah <164001590+MohammadRiswandaAlamsyah@users.noreply.github.com>
2024-06-10 15:24:26 +0800

    Add files via upload

---
+++ penggajian/routes.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = "";
+}
+
+switch ($page) {
+    case "";
+    case "dashboard";
+        include "pages/dashboard.php";
+        break;
+    case "bagian";
+        include "pages/bagian.php";
+        break;
+    case "bagiantambah";
+        include "pages/bagiantambah.php";
+        break;
+    case "bagianhapus";
+        include "pages/bagianhapus.php";
+        break;
+    case "bagianubah";
+        include "pages/bagianubah.php";
+        break;
+    default;
+        include "pages/404.php";
+}
+?>
